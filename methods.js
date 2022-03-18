0581dec9f6886dd482fd4a591dd71a2a4e346118
codeironside <fury25423@gmail.com>
2022-03-18 11:53:56 +0100

    keep them safe here

---
+++ methods.js
@@ -0,0 +1,19 @@
+const add = function(x, y){
+    return x + y;
+}
+const multiply = function(x, y){
+    return x * y;
+}
+const divide = function(x, y){
+    return x / y;
+}
+const substract = function(x, y){
+    return x - y;
+}
+const thing = {
+    multiplyMethod: multiply,
+    addMethod: add,
+    substractMethod: substract,
+    divideMethod: divide 
+ 
+}
